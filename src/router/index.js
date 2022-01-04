b7f45bffd85544b912625be59b07147161eadb09
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-01-04 14:28:29 +0300

    start adding desktop header

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,50 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  // {
+  //   path: "/goruntulu-konusma",
+  //   name: "Goruntulu-konusma-desktop",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  // },
+  {
+    path: "/desk",
+    name: "Desktop",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/goruntulu-konusma/Desktop.vue"
+      ),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/goruntulu-konusma/Desktop.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="goruntulu-konusma-desktop">
+    <div class="header">
+      <img class="logo" src="../../assets/logo.svg" alt="" />
+      <img src="../../assets/online-islemler.svg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.goruntulu-konusma-desktop {
+  background: #32a5df;
+  height: 100vh;
+  width: 100%;
+}
+</style>
