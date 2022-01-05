6a589a6aa192dd49eae0e91f73883aa199768146
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-01-05 17:17:58 +0300

    add big white box

--- src/views/goruntulu-konusma/BigWhiteBox.vue
+++ src/views/goruntulu-konusma/BigWhiteBox.vue
@@ -30,6 +30,7 @@
         </svg>
       </div>
     </div>
+    <div class="bigWhiteBox"></div>
   </div>
 </template>
 
@@ -40,13 +41,27 @@ export default {};
 <style scoped>
 .bigWhiteBoxComponent {
   width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 .titlePart {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  border: 1px solid black;
+  /* border: 1px solid black; */
   width: 100%;
+  padding: 2em;
+}
+.bigWhiteBox {
+  /* border: 1px solid black; */
+  width: 90%;
+  height: 90%;
+  background-color: white;
+  padding: 2em;
+  border-radius: 3%;
 }
 </style>
