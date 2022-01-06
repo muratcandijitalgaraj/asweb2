db67527d3bc9e6b1bed18252759787ecb2f85993
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-01-06 16:36:02 +0300

    add vue responsive library

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
+import responsive from "vue-responsive";
+Vue.use(responsive);
+
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
