1a4557b1911ef5e05d7700296e7a27d23a0e59ec
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-01-10 05:11:18 +0300

    fallback, gonna try without bootstrap

--- src/views/goruntulu-konusma/tablet-whiteBoxes.vue
+++ src/views/goruntulu-konusma/tablet-whiteBoxes.vue
@@ -1,22 +1,38 @@
 <template>
-  <div class="whiteBoxesContainer">
-    <div class="individualBox">
-      <img class="boxItem" src="../../assets/calendar-1.svg" alt="" />
+  <div class="">
+    <b-container class="bv-example-row">
+      <b-row class="whiteBoxesContainer">
+        <b-col cols="6">
+          <div class="individualBox">
+            <img class="boxItem" src="../../assets/calendar-1.svg" alt="" />
 
-      <p class="boxPara">Hastane Randevusu Alın</p>
-    </div>
-    <div class="individualBox">
-      <img class="boxItem" src="../../assets/camera.svg" alt="" />
-      <p class="boxPara">Görüntülü Görüşme Randevusu Alın</p>
-    </div>
-    <div class="individualBox">
-      <img class="boxItem" src="../../assets/heart-circle.svg" alt="" />
-      <p class="boxPara">Check-Up Başvurusu Yapın</p>
-    </div>
-    <div class="individualBox">
-      <img class="boxItem" src="../../assets/bitcoin-convert.svg" alt="" />
-      <p class="boxPara">Tıbbi İkinci Görüş Formu</p>
-    </div>
+            <p class="boxPara">Hastane Randevusu Alın</p>
+          </div></b-col
+        >
+        <b-col cols="6">
+          <div class="individualBox">
+            <img class="boxItem" src="../../assets/camera.svg" alt="" />
+            <p class="boxPara">Görüntülü Görüşme Randevusu Alın</p>
+          </div></b-col
+        >
+        <b-col cols="6">
+          <div class="individualBox">
+            <img class="boxItem" src="../../assets/heart-circle.svg" alt="" />
+            <p class="boxPara">Check-Up Başvurusu Yapın</p>
+          </div></b-col
+        >
+        <b-col cols="6">
+          <div class="individualBox">
+            <img
+              class="boxItem"
+              src="../../assets/bitcoin-convert.svg"
+              alt=""
+            />
+            <p class="boxPara">Tıbbi İkinci Görüş Formu</p>
+          </div></b-col
+        >
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -26,31 +42,32 @@ export default {};
 
 <style scoped>
 .whiteBoxesContainer {
-  height: 12.5rem;
+  /* height: 12.5rem; */
   border-bottom: 1px solid #dae9f1;
-  width: 100%;
-  /* border: 1px solid black; */
+  width: auto;
   display: flex;
-  justify-content: space-evenly;
+  flex-direction: row;
+  justify-content: center;
   align-items: center;
-  /* padding-bottom: 1.5em; */
-  /* margin-top: 1.5em;
-
-  margin-bottom: 1.5em; */
+  border: 1px solid black;
+  padding: 1em;
 }
 .individualBox {
   /* border: 1px solid black; */
-  width: 100%;
-  height: 65%;
+  width: 210px;
+  height: 130px;
   background: #ffffff;
   box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.03);
   border-radius: 10px;
-  margin-right: 1em;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
   padding: 1em;
+  padding-right: 0;
+  /* this makes the text looks better */
+  padding-right: 2em;
+  margin-bottom: 1.5em;
 }
 .boxItem {
   margin-bottom: 0.8em;
