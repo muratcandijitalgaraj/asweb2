08553a9fe7af535a6229e31f0e117ae61910dd59
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-01-16 17:45:59 +0300

    add sol bulut

--- src/views/randevulariniz/Randevulariniz.vue
+++ src/views/randevulariniz/Randevulariniz.vue
@@ -5,7 +5,10 @@
         <img src="../../assets/giris/logo.svg" alt="Anadolu Logo" />
       </div>
       <div class="online-islemler">ONLINE İŞLEMLER</div>
-      <div class="bulutlar"></div>
+      <div class="bulutlar">
+        <div class="sol-bulut"></div>
+        <div class="sag-bulut"></div>
+      </div>
     </div>
     <div class="greyBox"></div>
   </div>
@@ -38,14 +41,36 @@ export default {};
   /* identical to box height, or 106% */
 
   text-align: center;
-
+  background-repeat: no-repeat;
   color: #ffffff;
 }
+.bulutlar {
+  width: 421px;
+  height: 100%;
+  /* border: 1px solid black; */
+
+  background: linear-gradient(
+      180deg,
+      #33a5df 16.85%,
+      rgba(51, 165, 223, 0) 58.86%
+    ),
+    url("../../assets/bulut.svg");
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.sol-bulut {
+  width: 66px;
+  height: 111px;
+
+  background: linear-gradient(90deg, #32a5df 0%, rgba(50, 165, 223, 0) 100%);
+}
+
 .greyBox {
   width: 1250px;
   height: 1035px;
-  /* background: #f2f8fb; */
+  background: #f2f8fb;
   border-radius: 22px;
-  background: black;
+  /* background: black; */
 }
 </style>
