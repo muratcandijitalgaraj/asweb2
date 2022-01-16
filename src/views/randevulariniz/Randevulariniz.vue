c3777a3892c425ab294b0da0bc58e06ec357ec45
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-01-16 17:28:25 +0300

    adding logo part

--- src/views/randevulariniz/Randevulariniz.vue
+++ src/views/randevulariniz/Randevulariniz.vue
@@ -1,9 +1,51 @@
 <template>
-  <div id="randevulariniz-component"><h1>hey</h1></div>
+  <div id="randevulariniz-component">
+    <div class="logoPart">
+      <div class="logo">
+        <img src="../../assets/giris/logo.svg" alt="Anadolu Logo" />
+      </div>
+      <div class="online-islemler">ONLINE İŞLEMLER</div>
+      <div class="bulutlar"></div>
+    </div>
+    <div class="greyBox"></div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+#randevulariniz-component {
+  width: 1920px;
+  height: 1264px;
+  background: #32a5df;
+}
+.logoPart {
+  display: flex;
+  flex-direction: row;
+  /* border: 1px solid black; */
+}
+.online-islemler {
+  width: 152px;
+  height: 18px;
+
+  font-family: Nunito Sans;
+  font-style: normal;
+  font-weight: 800;
+  font-size: 17px;
+  line-height: 18px;
+  /* identical to box height, or 106% */
+
+  text-align: center;
+
+  color: #ffffff;
+}
+.greyBox {
+  width: 1250px;
+  height: 1035px;
+  /* background: #f2f8fb; */
+  border-radius: 22px;
+  background: black;
+}
+</style>
